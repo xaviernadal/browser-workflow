e7a5a463a0e3f77f48aec566720f2a9bcb7f6949
manudiv16 <manudiv16@manudiv16-pc>
2020-03-11 22:57:18 +0100

    code connection with ContactDataBase

--- work.py
+++ work.py
@@ -1,56 +1,47 @@
 #!/usr/bin/env python3
 
+import argparse  # per llegir arguments
 import os
-import argparse
-import webbrowser
 import sys
-import pyautogui
-from tkinter import Tk
+import webbrowser
 from time import sleep
+from tkinter import Tk  # clipboard
+
+import pyautogui  # automatitzar hotkeys
+
+from ges_data_base import ContactDataBase
 
 SLEEP_TIME = 0.1
 MAX_DUPLICATE = 2
 
-work = {}
+src = "Database/"
 
 
-def prepareWork():
-    subjectList = open('workspaces.txt', 'r')
-    print("List:")
-    for key in subjectList:
-        key = key.strip()
-        subjectFile = key + "Links.txt"
+class Database(object):
 
-        work.update({key: subjectFile})
-        print("- " + key)
-    print(work)
-    if bool(work) == False:
-        print("You need to save a workspace first!")
+    def get_list_workspaces_generator(self):
+        return (i for i in os.listdir(src))
+
+    def get_list_workspaces(self):
+        return set([i for i in self.get_list_workspaces_generator()])
 
 
-def writeNewWorkspace(subject):
-    subjectFile = subject + "Links.txt"
-    spaces = open('workspaces.txt', 'w')
-    spaces.write(subject)
-    work.update({subject: subjectFile})
-
-
-links = []
-
-
-def writeLinks(subject):
-    pages = work[subject]
-    f = open(pages, 'w')
-    f.write('\n'.join(links))
-    f.close()
+def prepareWork():  # lista los archivos que tenemos en Database
+    database = Database()
+    database_work = database.get_list_workspaces_generator()
+    for workspase in database_work:
+        print(workspase)
+        return
+    print("You need to save a workspace first!")
 
 
 def copyUrls():
-    pyautogui.hotkey('winleft', '7')
+    pyautogui.hotkey('winleft', '1')
     sleep(SLEEP_TIME)
     pyautogui.hotkey('ctrl', '1')
     duplicate_count = 0
     tabs_count = 0
+    links = set()
     while duplicate_count < MAX_DUPLICATE:
         pyautogui.hotkey('alt', 'd')
         sleep(SLEEP_TIME)
@@ -60,19 +51,19 @@ def copyUrls():
         if link in links:
             duplicate_count += 1
         else:
-            links.append(link)
+            links.update(link)
         sleep(SLEEP_TIME)
         pyautogui.hotkey('ctrl', 'tab')
         sleep(SLEEP_TIME)
         tabs_count += 1
     for _ in range(tabs_count):
         pyautogui.hotkey('ctrl', 'w')
+    return links
 
 
-def openWork(subject):
-    pages = work[subject]
-
-    urls = open(pages, 'r')
+def openWork(filename):
+    data = ContactDataBase(filename)
+    urls = data.read_links()
     for line in urls:
         webbrowser.open(line, new=0, autoraise=True)
     sleep(5)
@@ -80,20 +71,20 @@ def openWork(subject):
 
 
 def readInput():
-    subject = input().split()
-    first = subject[0]
+    search_filename = input().split()
+    first = search_filename[0]
+    database = Database()
+    database_work = database.get_list_workspaces()
     if first == "save":
-        subject = subject[1]
-        if not subject in work.keys():
+        filename = search_filename[1]
+        if not filename in database_work:
             line = "\nCreating workspace named {}...\n"
-            print(line.format(subject))
-            writeNewWorkspace(subject)
-
-        copyUrls()
-        writeLinks(subject)
-
-    elif not first in work.keys():
-        line = "\nThere's no subject named {}\n\nFor more info, write 'work -h'"
+            print(line.format(filename))
+            data = ContactDataBase(filename)
+        urls = copyUrls()
+        data.modify(urls)
+    elif not first in database_work:
+        line = "\nThere's no filename named {}\n\nFor more info, write 'work -h'"
         print(line.format(first))
     else:
         openWork(first)
